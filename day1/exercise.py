a35418ac21f736b0c9307b4a09cecda9f77a82fc
Rian Katoen <rian.katoen@aaa-riskfinance.nl>
2022-12-02 04:29:45 +0100

    Day 1 - List comprehension.

--- day1/exercise.py
+++ day1/exercise.py
@@ -1,9 +1,9 @@
 from input import input
 
-elves = sorted(map(lambda c : sum(c), input("example")), reverse=True)
-print("example  1: ", elves[0])
-print("example  2: ", sum(elves[0:3]))
+elves = sorted([sum(c) for c in input("example")])
+print("example  1: ", elves[-1])
+print("example  2: ", sum(elves[-3:]))
 
-elves = sorted(map(lambda c : sum(c), input("input")), reverse=True)
-print("exercise 1: ", elves[0])
-print("exercise 2: ", sum(elves[0:3]))
+elves = sorted([sum(c) for c in input("input")])
+print("exercise 1: ", elves[-1])
+print("exercise 2: ", sum(elves[-3:]))
