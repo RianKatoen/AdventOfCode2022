a72ff0d34c8d89b97a65349d587b0bc1aace7092
Rian Katoen <rian.katoen@aaa-riskfinance.nl>
2022-12-06 14:04:13 +0100

    Day 6 - Meh, implementation is not up to my prefered way. Sets seem overkill...

---
+++ day6/puzzle.py
@@ -0,0 +1,23 @@
+# Common.
+def input(file):
+    return [line.strip() for line in open("day6/" + file + ".txt", "r")]
+
+def marker(message, n = 4):
+    i = n
+    while i < len(message):
+        if len(message[(i - n):i]) == len(set(message[(i - n):i])):
+            return i
+        else:
+            i += 1
+
+example = input("example")
+
+for i in range(4):
+    print("example 1." + str(i+1) + ": ", marker(example[i]))
+
+print("part 1: ", marker(input("input")[0]))
+
+for i in range(4):
+    print("example 2." + str(i+1) + ": ", marker(example[i], 14))
+
+print("part 2: ", marker(input("input")[0], 14))
